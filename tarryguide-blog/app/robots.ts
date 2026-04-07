e4f922cfa048992a36706530da418c1fe2f8e723
kimjonghoon1306 <s965300111@gmail.com>
2026-04-08 05:06:25 +0900

    Delete tarryguide-blog directory

--- tarryguide-blog/app/robots.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataRoute } from "next";
-export default function robots(): MetadataRoute.Robots {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://tarryguide.com";
-  return {
-    rules: { userAgent: "*", allow: "/", disallow: ["/admin", "/api/"] },
-    sitemap: `${siteUrl}/sitemap.xml`,
-  };
-}
